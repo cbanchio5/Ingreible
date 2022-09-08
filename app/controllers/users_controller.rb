38cf3b392572a58275ebb0cec18997b3039a9210
Gabriele Benevenia <gabrib1985@protonmail.com>
2022-09-08 22:06:09 +0200

    favourites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    authorize @user
+    if current_user.id == @user.id
+      authorize @user
+    else
+      redirect_to recipes_path
+    end
   end
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,5 +1,5 @@
 class Favourite < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
-
+  validates_uniqueness_of :user_id, scope: [ :recipe_id]
 end
