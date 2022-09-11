91bf803c7ea5498710449d09a16324cf6a15e22a
cesar banchio <cbanchio5@gmail.com>
2022-09-11 14:11:56 +0200

    added photo updating to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :photo])
   end
   include Pundit
 
