1b0da2af297cbc5b136e9119b368f6f282536d4e
cesar banchio <cbanchio5@gmail.com>
2022-09-14 17:45:48 +0200

    next functionality working

--- app/javascript/controllers/modalcooking_controller.js
+++ app/javascript/controllers/modalcooking_controller.js
@@ -6,16 +6,14 @@ export default class extends Controller {
     initialize() {
       let step = this.indexTarget.innerText
       step = step.replace(/\D/g, '') * 1
-      console.log(this.contentTarget.querySelectorAll('.step')[step -1])
-      this.contentTarget.querySelectorAll('.step')[step -1].classList.remove('inactive--div')
-      console.log(this.contentTarget.querySelectorAll('.step')[step -1])
-      // if (step === 1) {
-      //   this.previousTarget.setAttribute("disabled", "")
-      //   this.contentTarget.querySelectorAll('p')[step -1].classList.remove('inactive--div')
 
-      // } else {
-      //   this.previousTarget.removeAttribute('disbaled')
-      // }
+      this.contentTarget.querySelectorAll('.step')[step -1].classList.remove('inactive--div')
+
+      if (step === 1) {
+        this.previousTarget.setAttribute("disabled", "")
+      } else {
+        this.previousTarget.removeAttribute('disbaled')
+      }
 
     }
 
@@ -35,12 +33,39 @@ export default class extends Controller {
 
   next() {
 
+    //Get total steps of recipe
+    let totalSteps = this.contentTarget.querySelectorAll('.step').length
 
+    //Step text for div
     let step = this.indexTarget.innerText
-    let step_number = step.replace(/\D/g, '') * 1
-    let next_step = step_number + 1
-    step = step.replace("1", next_step)
-    this.indexTarget.innerText = step
+
+    //Extract number of String
+    let stepNumber = step.replace(/\D/g, '') * 1
+
+    let nextStep = stepNumber + 1
+
+    //Disable next button if current step is last step
+    if (nextStep === totalSteps) {
+      this.nextTarget.setAttribute("disabled", "")
+    }
+
+    //Able the previous button if not in first step
+
+    if(nextStep > 1) {
+      this.previousTarget.removeAttribute('disabled')
+    }
+
+    //Update step title
+
+    this.indexTarget.innerText = step.replace(stepNumber, nextStep)
+
+    //Add inactive class again to previous step
+    this.contentTarget.querySelectorAll('.step')[stepNumber-1].classList.toggle('inactive--div')
+    // this.contentTarget.querySelectorAll('.step')[stepNumber].classList.add('inactive--div')
+    this.contentTarget.querySelectorAll('.step')[stepNumber].classList.toggle('inactive--div')
+    //Remove inactive class to current step
+    // this.contentTarget.querySelectorAll('.step')[stepNumber + 1].classList.remove('inactive--div')
+
 
 
 
