6386cd92191e6549a03cad865af17c14b9b05c6a
cesar banchio <cbanchio5@gmail.com>
2022-09-24 13:13:04 +0200

    connected stimulus controller to star ratings

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -2,11 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
+  static targets = ["link1", "link2", "link3", "link4", "link5"]
+
   connect() {
     console.log("rating controller")
   }
 
-  getValue() {
-    console.log("This is were values will be")
+  getValue1() {
+    console.log(this.link1Target.value * 1)
+  }
+
+  getValue2() {
+    console.log(this.link2Target.value * 1)
+  }
+
+  getValue3() {
+    console.log(this.link3Target.value * 1)
+  }
+
+  getValue4() {
+    console.log(this.link4Target.value * 1)
+  }
+
+  getValue5() {
+    console.log(this.link5Target.value * 1)
   }
 }
